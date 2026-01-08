0fd20f47d31cec7eb16aad8b5e8f5bed3e585444
Mrudul1234 <mrudulmistry@gmail.com>
2026-01-08 22:52:35 +0530

    Co-authored-by: Dipanshuyadav2611 <Dipanshuyadav2611@users.noreply.github.com>

--- atm.py
+++ atm.py
@@ -4,6 +4,7 @@ class Account:
          self.account_holder_name = name
          self.pin = pin   
          self.balance = balance
+         self.access=False
 
     def deposit(self, amount):
         self.balance = self.balance + amount
@@ -20,7 +21,48 @@ class Account:
         print(f"current balance:{self.balance}")      
 
 
-acc1=Account(2313,"mrudul",2006,100000)
-acc1.deposit(2000)
-acc1.withdraw(6000)
-acc1.display_balance()
+# acc1=Account(2313,"mrudul",2006,100000)
+# acc1.deposit(2000)
+# acc1.withdraw(6000)
+# acc1.display_balance()
+
+def login(password) :
+        if(password==Account.pin):
+            Account.access=True
+        else :
+            Account.access=False
+        
+def main():
+    my_account = Account()
+    
+        
+    print("*******welcome*******")
+    userpass = int(input("Write your password"))
+
+    if(login(userpass)):
+        print("1. check balance")
+        print("2.deposit")
+        print("3.withdraw")
+        print("4.logout")
+
+        user_choice = input("enter your choice")
+
+        if user_choice == "1":
+            my_account.display_balance() 
+        elif user_choice == "2":
+            amount = input(f"enter the deposit amount:{amount}")
+        elif user_choice == "3":
+                 amount = input(f"enter withdrawal amount:{amount}") 
+        elif user_choice == "4":
+            print("logging out")
+            print("*****thank you*****")
+            #break 
+            return
+        else :
+             print("invalid opition")
+        
+        #return ayega yaha break only use in loops return means use consition se return ya stop kardena he 
+    else:
+        print("wrong password try again !")
+
+main()
