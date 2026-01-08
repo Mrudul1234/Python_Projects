d4b4c6c5181bc1998c7ee785b3a8d9d0d85db7c6
Mrudul1234 <mrudulmistry@gmail.com>
2026-01-09 00:03:51 +0530

    code updated

--- atm.py
+++ atm.py
@@ -2,9 +2,15 @@ class Account:
     def __init__(self,account_no, name, pin, balance = 0):
          self. account_number = account_no
          self.account_holder_name = name
-         self.pin = pin   
+         self.pin = pin 
          self.balance = balance
          self.access=False
+
+    def login(self,password):
+        if password==self.pin :
+            self.access=True
+        else:
+            self.access=False
 
     def deposit(self, amount):
         self.balance = self.balance + amount
@@ -18,37 +24,32 @@ class Account:
             print("insufficient fund")
         
     def display_balance(self):
-        print(f"current balance:{self.balance}")      
+        print(f"current balance:{self.balance}") 
+         
+#error smaj aaraha he tuje ??
+
+acc1=Account(2313,"mrudul",2006,100000)
 
 
-# acc1=Account(2313,"mrudul",2006,100000)
-# acc1.deposit(2000)
-# acc1.withdraw(6000)
-# acc1.display_balance()
-
-def login(password) :
-        if(password==Account.pin):
-            Account.access=True
-        else :
-            Account.access=False
-        
 def main():
-    my_account = Account()
+    #my_account = Account()
     
         
     print("*******welcome*******")
-    userpass = int(input("Write your password"))
+    userpass = int(input("Write your password :-"))
+    
+    acc1.login(userpass)
 
-    if(login(userpass)):
+    if(Account.login):
         print("1. check balance")
         print("2.deposit")
         print("3.withdraw")
         print("4.logout")
 
-        user_choice = input("enter your choice")
+        user_choice = input("enter your choice :- ")
 
         if user_choice == "1":
-            my_account.display_balance() 
+            Account.display_balance() 
         elif user_choice == "2":
             amount = input(f"enter the deposit amount:{amount}")
         elif user_choice == "3":
@@ -61,7 +62,7 @@ def main():
         else :
              print("invalid opition")
         
-        #return ayega yaha break only use in loops return means use consition se return ya stop kardena he 
+             
     else:
         print("wrong password try again !")
 
